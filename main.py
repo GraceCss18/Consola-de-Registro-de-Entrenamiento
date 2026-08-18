211fcb0dd25d7f7bd78b52aabb451ee977380638
GraceCss18 <higraciela@hotmail.com>
2026-08-18 11:08:04 -0500

    primer comentario o commit

---
+++ main.py
@@ -0,0 +1,5 @@
+#git add: para agregar los archivos que estan untracked en el respositorio
+#git unit para crear un nuevo repositorio 
+#git status para ver los repositorios
+#git restore es para eliminar los cambios que haya realizado en un archivo
+#con git add . se guardan todo lo que se agrego en todos los archivos
